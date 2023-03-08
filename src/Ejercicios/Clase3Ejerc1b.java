2794bfb0bb1d64fc4cab0cc8a58cfae2f5d591e0
marianolaredo <mariano.laredo@gmx.com>
2023-03-08 11:21:52 -0300

    actualizacion

--- src/Ejercicios/Clase3Ejerc1b.java
+++ src/Ejercicios/Clase3Ejerc1b.java
@@ -4,9 +4,8 @@ public class Clase3Ejerc1b {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        //Ordenar de mayor a menor
-		int numeros[] = new int[] {18, 24, 9};
-		
+        //Ordenar de mayor a menor o menor a mayor
+		int numeros[] = new int[] {18, 24, 9};		
 		for(int i = 0; i < numeros.length - 1; i++) {
 			for(int j = 0; j < numeros.length - 1; j++) {
 				if(numeros[j] < numeros[j + 1]) {
