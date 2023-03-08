a19327109cf5a46dfa029e83ea7b2f2f1be04e70
marianolaredo <mariano.laredo@gmx.com>
2023-03-08 11:22:01 -0300

    actualizacion

--- src/Ejercicios/Clase3Ejerc1c.java
+++ src/Ejercicios/Clase3Ejerc1c.java
@@ -4,6 +4,7 @@ public class Clase3Ejerc1c {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Devuelve suma de 3 INT mayores a otro INT
 int numeros [] = new int[] {10,20,30};
 int suma = 0;
 int mayorA = 3;
